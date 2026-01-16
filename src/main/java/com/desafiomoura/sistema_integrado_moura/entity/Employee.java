e2b9bb71d4338967823ab5f50ed96fd5a6e3f7d8
AbnerBarretto <abnerlc4@gmail.com>
2026-01-16 18:01:09 -0300

    Criando estrutura inicial das classes Employee e WorkRecord

--- src/main/java/com/desafiomoura/sistema_integrado_moura/entity/Employee.java
+++ src/main/java/com/desafiomoura/sistema_integrado_moura/entity/Employee.java
@@ -1,4 +1,8 @@
 package com.desafiomoura.sistema_integrado_moura.entity;
 
 public class Employee {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
 }
--- src/main/java/com/desafiomoura/sistema_integrado_moura/entity/WorkRecord.java
+++ src/main/java/com/desafiomoura/sistema_integrado_moura/entity/WorkRecord.java
@@ -1,4 +1,13 @@
 package com.desafiomoura.sistema_integrado_moura.entity;
 
+import java.time.LocalDateTime;
+
 public class WorkRecord  {
+    private Long id;
+    private LocalDateTime checkinTime;
+    private LocalDateTime checkoutTime;
+    private Long duration;
+
+    private Employee employee;
+
 }
