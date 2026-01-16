d855708f19cb20db53b46fdce3f5e67a617c4a05
AbnerBarretto <abnerlc4@gmail.com>
2026-01-16 19:12:38 -0300

    feat: implementação das entidades com Lombok repositórios

--- src/main/java/com/desafiomoura/sistema_integrado_moura/entity/Employee.java
+++ src/main/java/com/desafiomoura/sistema_integrado_moura/entity/Employee.java
@@ -1,8 +1,29 @@
 package com.desafiomoura.sistema_integrado_moura.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
+    @Column(unique = true)
     private String email;
+
     private String password;
+
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL)
+    private List<WorkRecord> workRecords;
 }
