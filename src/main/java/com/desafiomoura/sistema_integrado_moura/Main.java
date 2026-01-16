84529b4d80721aa7ad4c4cd9e5121704d082eabd
AbnerBarretto <abnerlc4@gmail.com>
2026-01-16 16:49:23 -0300

    Subindo projeto recuperado

---
+++ src/main/java/com/desafiomoura/sistema_integrado_moura/Main.java
@@ -0,0 +1,13 @@
+package com.desafiomoura.sistema_integrado_moura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
